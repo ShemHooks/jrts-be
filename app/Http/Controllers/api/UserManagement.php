56058167e8762123f33b634a2da521b712df88aa
unknown <shemregidor07@gmail.com>
2026-03-03 13:39:52 +0800

    Add User Management

--- app/Http/Controllers/api/UserManagement.php
+++ app/Http/Controllers/api/UserManagement.php
@@ -4,8 +4,36 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
+use App\Http\Controllers\api\BaseController as BaseController;
+use App\Models\User;
 
-class UserManagement extends Controller
+class UserManagement extends BaseController
 {
+    public function index(Request $request)
+    {
+        $number_per_page = $request->input('per_page', 10);
+        $keyword = $request->input('keyword');
+        $status = $request->input('status');
+        $role = $request->input('role');
+        $position = $request->input('position');
 
+        $users = User::with('office')
+            ->when($status, function ($query) use ($status) {
+                $query->where('status', $status);
+            })
+            ->when($role, function ($query) use ($role) {
+                $query->where('role', $role);
+            })
+            ->when($position, function ($query) use ($position) {
+                $query->where('position', $position);
+            })
+            ->when($keyword, function ($query) use ($keyword) {
+                $query->where('name', 'like', "%{$keyword}%")
+                    ->orWhere('email', 'like', "%{$keyword}%");
+            })->paginate((int) $number_per_page);
+
+        return $this->sendResponse($users, 'List of Users');
+
+    }
 }
